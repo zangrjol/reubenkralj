61aef09b469212459cbf3bee543a81c0c8f4ef4c
zangrjol <grjol.zan@gmail.com>
2019-04-12 23:58:41 +0200

    a few new functions

---
+++ data/download_data.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+data <- read_csv2("ichimoku/data1h.csv", col_names = TRUE,)
+data <- data %>%
+  mutate(start = as.POSIXct(as.character(start), format = "%d/%m/%Y %H:%M"))
+
+save(data, file = "ichimoku/data.rda")
--- ichimoku/analyse.R
+++ ichimoku/analyse.R
@@ -3,6 +3,7 @@ load("ichimoku/data.rda")
 
 
 # function to generate a new table with ichimoku columns
+
 ichimoku <- function(signal, base) {
     data %>% 
       mutate(
@@ -25,3 +26,79 @@ ichimoku <- function(signal, base) {
 #another column which tells when to buy/sell
 #1 is buy, -1 is sell
 
+
+ichimoku_signal <- function(signal, base, fee) {
+  
+  ichimoku_test <- ichimoku(signal, base)
+  
+  #signal column
+  ichimoku_test <- cbind(ichimoku_test, signal = 0)
+  position <- 1 #we start long
+  for (i in 1:nrow(ichimoku_test)) {
+    
+    if (ichimoku_test$diff[i] > 0 & position == -1) {
+      ichimoku_test$signal[i] = 1
+      position <- 1
+    } else if (ichimoku_test$diff[i] < 0 & position == 1) {
+      ichimoku_test$signal[i] = -1
+      position <- -1
+    } else {
+      ichimoku_test$signal[i] = ifelse(i > 0, ichimoku_test$signal[i-1], position)
+    }
+  }
+  
+  #pl column
+  ichimoku_test <- cbind(ichimoku_test, pl = 1)
+  for (i in 2:nrow(ichimoku_test)) {
+    if (ichimoku_test$signal[i-1] > 0 ) {
+      if (sign(ichimoku_test$signal[i]) == sign(ichimoku_test$signal[i-1])) { # substract fee if trade is conducted
+        ichimoku_test$pl[i] = (ichimoku_test$open[i]/ichimoku_test$open[i-1])*ichimoku_test$pl[i-1]
+      } else {
+        ichimoku_test$pl[i] = (ichimoku_test$open[i]/ichimoku_test$open[i-1]-fee)*ichimoku_test$pl[i-1]
+      }
+    } else {
+      if (sign(ichimoku_test$signal[i]) == sign(ichimoku_test$signal[i-1])) {
+        ichimoku_test$pl[i] = (ichimoku_test$open[i-1]/ichimoku_test$open[i])*ichimoku_test$pl[i-1]
+      } else {
+        ichimoku_test$pl[i] = (ichimoku_test$open[i-1]/ichimoku_test$open[i]-fee)*ichimoku_test$pl[i-1]
+      }
+    }
+  }
+  
+  ichimoku_test
+}
+position <- 1 #we start long
+for (i in 1:nrow(ichimoku_test)) {
+
+  if (ichimoku_test$diff[i] > 0 & position == -1) {
+    ichimoku_test$signal[i] = 1
+    position <- 1
+  } else if (ichimoku_test$diff[i] < 0 & position == 1) {
+    ichimoku_test$signal[i] = -1
+    position <- -1
+  } else {
+    ichimoku_test$signal[i] = ifelse(i > 0, ichimoku_test$signal[i-1], position)
+  }
+}
+
+ichimoku_test <- cbind(ichimoku_test, pl = 1)
+fee <- 0.00025
+for (i in 2:nrow(ichimoku_test)) {
+  if (ichimoku_test$signal[i-1] > 0 ) {
+    if (sign(ichimoku_test$signal[i]) == sign(ichimoku_test$signal[i-1])) { # substract fee if trade is conducted
+      ichimoku_test$pl[i] = (ichimoku_test$open[i]/ichimoku_test$open[i-1])*ichimoku_test$pl[i-1]
+    } else {
+      ichimoku_test$pl[i] = (ichimoku_test$open[i]/ichimoku_test$open[i-1]-fee)*ichimoku_test$pl[i-1]
+    }
+  } else {
+    if (sign(ichimoku_test$signal[i]) == sign(ichimoku_test$signal[i-1])) {
+      ichimoku_test$pl[i] = (ichimoku_test$open[i-1]/ichimoku_test$open[i])*ichimoku_test$pl[i-1]
+    } else {
+      ichimoku_test$pl[i] = (ichimoku_test$open[i-1]/ichimoku_test$open[i]-fee)*ichimoku_test$pl[i-1]
+    }
+  }
+}
+
+
+test <- ichimoku_signal(15,16,0.00025)
+View(test)
